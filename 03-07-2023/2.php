e3b25b4cffcf85f5e12c2686ee3d95540447f173
2101031000166 <137051091+2101031000166@users.noreply.github.com>
2023-07-03 15:38:44 +0530

    day13

---
+++ 03-07-2023/2.php
@@ -0,0 +1,9 @@
+<?php
+$name = "Prince Patel";
+$number = 2101031000165;
+$department = "B.tech IT";
+
+echo "Name: " . $name . "<br>";
+echo "Number: " . $number . "<br>";
+echo "Department: " . $department . "<br>";
+?>
